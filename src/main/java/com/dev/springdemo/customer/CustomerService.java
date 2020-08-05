3eff89d05c0ed3688e180e7120b0ddaa8cf14942
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2020-08-05 23:35:04 +0530

    added a logger to the customer service

--- src/main/java/com/dev/springdemo/customer/CustomerService.java
+++ src/main/java/com/dev/springdemo/customer/CustomerService.java
@@ -1,7 +1,9 @@
 package com.dev.springdemo.customer;
 
 import com.dev.springdemo.customer.model.Customer;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.Optional;
  * Created by sunitc on 4/19/18.
  */
 @Service
+@Log4j2
 public class CustomerService {
 
     //Autowiring the repository
@@ -20,17 +23,21 @@ public class CustomerService {
     private CustomerRepository customerRepository;
 
     //Get customer by Id
+    @Cacheable()
     public Optional<Customer> getCustomerById(Long customerId) {
-       return customerRepository.findById(customerId);
+        log.info("Fetching customer by id: {}", customerId);
+        return customerRepository.findById(customerId);
     }
 
     //Create the customer
     public Customer create(Customer customer) {
+        log.info("Creating a new customer with emailAddress: {}", customer.getEmailAddress());
         return customerRepository.save(customer);
     }
 
     //Update the customer
     public Customer update(Customer customer) {
+        log.info("Updating a customer with id: {}", customer.getId());
         Optional<Customer> optionalCustomer = customerRepository.findById(customer.getId());
         if(optionalCustomer.isEmpty()) {
             throw new RuntimeException("Cannot find the customer by id " + customer.getId());
